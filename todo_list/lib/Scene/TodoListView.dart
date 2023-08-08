4706131857f7094b63361d274929f1fcc68e122c
Byung jin <bjcho1503@naver.com>
2023-08-08 15:33:43 +0900

    프로젝트 세팅

---
+++ todo_list/lib/Scene/TodoListView.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoListView extends StatelessWidget {
+  const TodoListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const
+  }
+}
