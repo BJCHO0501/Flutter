2b0ba2899415918deb1fe3bc7717d7ce63096cf2
Byung jin <bjcho1503@naver.com>
2023-08-09 18:22:15 +0900

    todo list 체크와 입력기능 구현

--- todo_list/lib/Scene/Cell/ListCell.dart
+++ todo_list/lib/Scene/Cell/ListCell.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ListCell extends StatelessWidget {
+class ListCell extends StatefulWidget {
   // const ListCell ({super.key});
-  const ListCell(this.content);
+  ListCell(this.content);
   final String content;
+  bool isCheck = false;
 
+  @override
+  State<ListCell> createState() => _ListCellState();
+}
+
+class _ListCellState extends State<ListCell> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,7 +21,22 @@ class ListCell extends StatelessWidget {
         color: Colors.amberAccent,
         borderRadius: BorderRadius.all(Radius.circular(10))
       ),
-      child: Text(content),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(widget.content),
+          Expanded(child: SizedBox()),
+          IconButton(
+            onPressed: (){
+              setState(() {
+                widget.isCheck = !widget.isCheck;
+              });
+            },
+            icon: Icon(Icons.check),
+            color: widget.isCheck ? Colors.green : Colors.black,
+          )
+        ],
+      ),
       padding: EdgeInsets.all(10),
     );
   }
--- todo_list/lib/Scene/TodoListView.dart
+++ todo_list/lib/Scene/TodoListView.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/Resource/Fonts.dart';
 import 'package:todo_list/Scene/Cell/ListCell.dart';
 
-class TodoListView extends StatelessWidget {
+class TodoListView extends StatefulWidget {
   const TodoListView({super.key});
+
+  @override
+  State<TodoListView> createState() => _TodoListViewState();
+}
+
+class _TodoListViewState extends State<TodoListView> {
+  List<String> _contents = [];
+  String _inputText = "";
+
+  _TodoListViewState() {
+    this._contents.add("hello!!");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +25,9 @@ class TodoListView extends StatelessWidget {
         ),
       ),
       body: ListView.separated(
-        itemCount: 10,
+        itemCount: _contents.length,
         itemBuilder: (BuildContext context, int index) {
-          return ListCell('hellw!!');
+          return ListCell(_contents[index]);
         },
         separatorBuilder: (BuildContext context, int index) {
           return SizedBox(height: 10);
@@ -26,7 +37,60 @@ class TodoListView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
         onPressed: (){
-
+          showDialog(
+              context: context,
+              barrierDismissible: true,
+              builder: (BuildContext context) {
+                return Dialog(
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Colors.white
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          onChanged: (String text) {
+                            this._inputText = text;
+                          },
+                          decoration: InputDecoration(
+                            labelText: '할일',
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              borderSide: BorderSide(width: 1, color: Colors.grey)
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                borderSide: BorderSide(width: 1, color: Colors.grey)
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: (){
+                              setState(() {
+                                Navigator.of(context).pop();
+                                if (!_inputText.isEmpty) {
+                                  this._contents.add(_inputText);
+                                  _inputText = "";
+                                }
+                              });
+                            },
+                            child: Text(
+                              '확인',
+                              style: TextStyle(color: Colors.white),
+                            )
+                        )
+                      ],
+                    ),
+                  ),
+                  insetPadding: EdgeInsets.fromLTRB(20, 150, 20, 200),
+                );
+              }
+          );
         },
         child: Icon(Icons.add),
       ),
